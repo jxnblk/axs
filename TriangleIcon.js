c04b13664a8cc526fdd0bac5e649dcae337c820e
Brent Jackson <jxnblk@gmail.com>
2016-11-15 22:04:14 -0500

    Updates

--- TriangleIcon.js
+++ TriangleIcon.js
@@ -18,13 +18,13 @@ const TriangleIcon = () => (
   <Box>
     <style dangerouslySetInnerHTML={{ __html: css }} />
     <Box is='svg'
-      viewBox='0 0 32 32'
+      viewBox='0 0 32 24'
       width='512'
-      height='512'
+      height='384'
       style={sx.root}>
-      <rect width='32' height='32' fill='#be4bdb' />
-      <g transform='translate(0 8)'>
-        <Logo size={16} />
+      <rect width='32' height='24' fill='#be4bdb' />
+      <g transform='translate(0 6)'>
+        <Logo size={12} />
       </g>
     </Box>
   </Box>
