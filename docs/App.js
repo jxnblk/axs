bce79d05b97ef0c536489f80a8d32e397c2155cb
Brent Jackson <jxnblk@gmail.com>
2016-11-16 08:40:36 -0500

    Add usage section

--- docs/App.js
+++ docs/App.js
@@ -4,6 +4,7 @@ import { withContext } from 'recompose'
 import Nav from './Nav'
 import Header from './Header'
 import Features from './Features'
+import Usage from './Usage'
 import Examples from './Examples'
 import Cta from './Cta'
 import Footer from './Footer'
@@ -12,6 +13,7 @@ const App = () => (
   <div>
     <Nav />
     <Header />
+    <Usage />
     <Features />
     <Examples />
     <Cta />
