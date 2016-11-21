a32b6abe32bfee5e2993b602b16aa7a7d39e0e24
Brent Jackson <jxnblk@gmail.com>
2016-11-20 20:26:09 -0500

    Updates

--- html.js
+++ html.js
@@ -17,7 +17,7 @@ module.exports = ({
 <style>*{box-sizing:border-box}body{ font-family:-apple-sytem,BlinkMacSystemFont,sans-serif; line-height:1.5; margin:0 }code{font-family:Menlo,monospace;font-size:87.5%}</style>
 <style>${css}</style>
 <div id='app'>${app}</div>
-<script src='${basehref}bundle.js'></script>
+<script src='${basehref}/bundle.js'></script>
 <script id='twitter-wjs' src='https://platform.twitter.com/widgets.js' async></script>
 <script>
 (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create", "UA-4603832-6", "auto");ga("send", "pageview");
