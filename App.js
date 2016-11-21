6f4f5ec159b304b434334f0d71a1bff328d48633
Brent Jackson <jxnblk@gmail.com>
2016-11-20 20:24:02 -0500

    Updates

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
   render () {
     const { path } = this.state
 
-    const view = path === '/ui' ? <UI /> : <Index />
+    const view = /ui$/.test(path) ? <UI /> : <Index />
 
     return (
       <div>
