7fad7ccdce9dbdf06297cc8517f9b645df1c338d
Brent Jackson <jxnblk@gmail.com>
2017-05-26 10:46:04 -0400

    Add tests

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 const test = require('ava')
 const parse = require('./src/parse')
 
-const props = {
-  type: 'number',
-  value: 32
-}
-
 test('exports a function', t => {
   t.is(typeof parse, 'function')
 })
 
 test('accepts props object and returns an object', t => {
+  const props = {
+    type: 'number',
+    value: 32
+  }
   const parsed = parse(props)
   t.is(typeof parsed, 'object')
   t.deepEqual(parsed, props)
@@ -49,11 +48,13 @@ test('parses space values', t => {
 test('parses shorthand props', t => {
   const a = parse({
     m: 0,
+    mb: 2,
     px: 5,
   })
   t.is(typeof a, 'object')
   t.deepEqual(a, {
     margin: 0,
+    marginBottom: 16,
     paddingLeft: 5,
     paddingRight: 5
   })
@@ -80,13 +81,33 @@ test('parses boolean shorthands', t => {
   })
 })
 
-test('parses margin and padding props', t => {
+test('parses negative margin props', t => {
   const a = parse({
-    margin: 0,
-    padding: 2,
-    paddingLeft: 4,
-    m: 2,
-    mt: 3
+    marginLeft: -1,
+    marginRight: -2
   })
-  t.is(typeof a, 'object')
+  t.deepEqual(a, {
+    marginLeft: -8,
+    marginRight: -16,
+  })
+})
+
+test('parses negative shorthands', t => {
+  const a = parse({
+    mx: -3
+  })
+  t.deepEqual(a, {
+    marginLeft: -32,
+    marginRight: -32
+  })
+})
+
+test('parses negative boolean shorthands', t => {
+  const a = parse({
+    'mx-3': true
+  })
+  t.deepEqual(a, {
+    marginLeft: -32,
+    marginRight: -32
+  })
 })
